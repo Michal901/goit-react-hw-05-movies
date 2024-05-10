d11611f327702c83778481805313dec87783e041
Michal901 <momot1122@wp.pl>
2024-05-10 15:18:17 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import { Home } from './Home/Home';
+import { Movies } from './Movies/Movies';
+import { MovieDetails } from './MovieDetails/MovieDetails';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
 
 export const App = () => {
   return (
@@ -7,7 +12,7 @@ export const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast " element={<Team />} />
+          <Route path="cast " element={<Cast />} />
           <Route path="reviews " element={<Reviews />} />
         </Route>
         <Route path="*" element={<Home />} />
