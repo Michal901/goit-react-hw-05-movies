b420e2f8d4ddb33af5cb09cced4d5917dc7ba7b5
Michal901 <momot1122@wp.pl>
2024-05-10 19:28:58 +0200

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,25 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import { API_KEY } from 'constants/API_KEY/ApiKey';
+
 export const Home = () => {
-  const [trendingMovies, setTrendingMovies] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrendingMovies = () => {
+      try {
+        const response = axios.get(
+          `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+        );
+        setTrendingMovies(response.data.results);
+        console.log(trendingMovies);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTrendingMovies();
+  }, []);
 
   return (
     <div>
