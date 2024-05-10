07a8f971cd5702cc2cb2639b76166a2d01d98910
Michal901 <momot1122@wp.pl>
2024-05-10 15:15:56 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,17 @@
 import { Routes, Route, Link } from 'react-router-dom';
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast " element={<Team />} />
+          <Route path="reviews " element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
