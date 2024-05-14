7bd4c740ea9f3d7ab8f175f04d5f1ee40d398a29
Michal901 <momot1122@wp.pl>
2024-05-14 08:42:06 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,3 +1,39 @@
+import axios from 'axios';
+import { API_KEY } from 'constants/API_KEY/ApiKey';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export const Cast = () => {
-  return <>Podstrona cast</>;
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`
+        );
+        setMovieDetails(response.data.cast);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [movieId]);
+
+  return (
+    <>
+      {movieDetails.map(movie => (
+        <li key={movie.id}>
+          <img src={3} alt={movie.name} />
+          <p>{movie.name}</p>
+          <div>
+            <p>Character:</p>
+            <p>{movie.character}</p>
+          </div>
+        </li>
+      ))}
+    </>
+  );
 };
