cf9fa1b6aee62109a7f33347d5082a7f64449041
Michal901 <momot1122@wp.pl>
2024-05-14 14:04:51 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,71 +1,20 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { API_KEY } from 'constants/API_KEY/ApiKey';
-import { DataFetcher } from 'components/DataFetcher/DataFetcher';
-
-export const Cast = () => {
-  const { movieId } = useParams();
-  const [movieDetails, setMovieDetails] = useState(null);
-
-  return (
-    <>
-      <h2>Cast</h2>
-      <DataFetcher
-        url={`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`}
-        dataType="cast"
-        setData={setMovieDetails}
-      />
-      <ul>
-        {movieDetails.map(movie => (
-          <li key={movie.id}>
-            <p>{movie.name}</p>
-            <img
-              src={`https://image.tmdb.org/t/p/w200/${movie.profile_path}`}
-              alt={movie.name}
-            />
-            <div>
-              <p>Character:</p>
-              <p>{movie.character}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
-
-// import axios from 'axios';
-// import { API_KEY } from 'constants/API_KEY/ApiKey';
-// import { useEffect, useState } from 'react';
+// import { useState } from 'react';
 // import { useParams } from 'react-router-dom';
+// import { API_KEY } from 'constants/API_KEY/ApiKey';
+// import { DataFetcher } from 'components/DataFetcher/DataFetcher';
 
 // export const Cast = () => {
 //   const { movieId } = useParams();
 //   const [movieDetails, setMovieDetails] = useState(null);
 
-//   useEffect(() => {
-//     const fetchMovieDetails = async () => {
-//       try {
-//         const response = await axios.get(
-//           `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`
-//         );
-//         setMovieDetails(response.data.cast);
-
-//         console.log(response.data);
-//       } catch (error) {
-//         console.error('Error fetching movie details:', error);
-//       }
-//     };
-
-//     fetchMovieDetails();
-//   }, [movieId]);
-
-//   if (!movieDetails) {
-//     return <div>Loading...</div>;
-//   }
-
 //   return (
 //     <>
+//       <h2>Cast</h2>
+//       <DataFetcher
+//         url={`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`}
+//         dataType="cast"
+//         setData={setMovieDetails}
+//       />
 //       <ul>
 //         {movieDetails.map(movie => (
 //           <li key={movie.id}>
@@ -84,3 +33,54 @@ export const Cast = () => {
 //     </>
 //   );
 // };
+
+import axios from 'axios';
+import { API_KEY } from 'constants/API_KEY/ApiKey';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+export const Cast = () => {
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`
+        );
+        setMovieDetails(response.data.cast);
+
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [movieId]);
+
+  if (!movieDetails) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <ul>
+        {movieDetails.map(movie => (
+          <li key={movie.id}>
+            <p>{movie.name}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w200/${movie.profile_path}`}
+              alt={movie.name}
+            />
+            <div>
+              <p>Character:</p>
+              <p>{movie.character}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
