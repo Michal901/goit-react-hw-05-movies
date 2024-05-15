2d87e00e4a4b118091d4cde072523685161b2707
Michal901 <momot1122@wp.pl>
2024-05-15 10:20:58 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,3 +32,34 @@ export const App = () => {
     </div>
   );
 };
+
+// import { lazy, Suspense } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import { SuspenseFallback } from '../components/SuspenseFallback/SuspenseFallback';
+
+// const Home = lazy(() => import('../pages/Home/Home'));
+// const Movies = lazy(() => import('../pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+// const Cast = lazy(() => import('./Cast/Cast'));
+// const Reviews = lazy(() => import('./Reviews/Reviews'));
+// const SharedLayout = lazy(() => import('./SharedLayout/SharedLayout'));
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Suspense fallback={<SuspenseFallback />}>
+//         <Routes>
+//           <Route path="/" element={<SharedLayout />}>
+//             <Route index element={<Home />} />
+//             <Route path="/movies" element={<Movies />} />
+//             <Route path="/movies/:movieId" element={<MovieDetails />}>
+//               <Route path="cast" element={<Cast />} />
+//               <Route path="reviews" element={<Reviews />} />
+//             </Route>
+//             <Route path="*" element={<Home />} />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+//     </div>
+//   );
+// };
---
+++ src/components/SuspenseFallback/SuspenseFallback.jsx
@@ -0,0 +1,7 @@
+export const SuspenseFallback = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  );
+};
